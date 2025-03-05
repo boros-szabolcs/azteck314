6cdd20673e00123893d0a58bf3d726cb3a4fc9f0
Boros Szabolcs <borosszabolcs@yahoo.com>
2025-03-05 20:49:18 +0200

    added jTable

--- application/views/recruitment_start.php
+++ application/views/recruitment_start.php
@@ -15,6 +15,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<!-- BootStrap -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+		
+		<!-- jTable -->
+		<script src="https://cdn.jsdelivr.net/npm/jtable@2.6.0/lib/jquery.jtable.min.js"></script>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/jtable@2.6.0/lib/themes/metro/blue/jtable.min.css" />
 	</head>
 	<body>
 		<div class="container">
