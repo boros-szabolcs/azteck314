beea2b2d64bc2419783b2aacda6e5adf0bed7e90
Boros Szabolcs <borosszabolcs@yahoo.com>
2025-03-05 14:38:53 +0200

    added skeleton for Recruitment
    skeleton for Recruitment includes controller, many views, and a third_party file

---
+++ application/controllers/Recruitment.php
@@ -0,0 +1,67 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Recruitment extends CI_Controller {
+	
+	public function __construct()
+	{
+		parent::__construct();
+		include APPPATH . 'third_party/recruitment/views.php';
+	}
+	
+	/**
+	 * Index Page for this controller.
+	 */
+	public function index()
+	{
+		$this->decide();
+	}
+	
+	private function decide()
+	{
+		if ($this->session->userdata('user')) { // TODO [boross] check for correct version
+			$this->dashboard();
+		} else {
+			if (!true) { // TODO [boross] check for correct version
+				$this->login();
+			} else {
+				$this->signup();
+			}
+		}
+	}
+	
+	private function dashboard()
+	{
+		$this->display_webpage('dashboard');
+	}
+	
+	private function login()
+	{
+		$this->display_webpage('login');
+	}
+	
+	private function signup()
+	{
+		$this->display_webpage('signup');
+	}
+	
+	private function notfound()
+	{
+		$this->display_webpage();
+	}
+	
+	private function display_webpage($page = 'notfound')
+	{
+		$this->load->view('recruitment_start');
+		$this->display_page('recruitment_' . $page);
+		$this->load->view('recruitment_end');
+	}
+	
+	private function display_page($page)
+	{
+		$this->load->view('recruitment_header');
+		$this->load->view('recruitment_navi');
+		$this->load->view($page);
+		$this->load->view('recruitment_footer');
+	}
+}
---
+++ application/third_party/recruitment/views.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ *
+ */
+
+// ------------------------------------------------------------------------
+
+if ( ! function_exists('xxxXXXxxx') )
+{
+	/**
+	 *
+	 */
+	function xxxXXXxxx($webpage = 'notfound')
+	{
+		echo ' // TODO [boross] rename function name and fill it with different content';
+	}
+}
+
+// ------------------------------------------------------------------------
+
