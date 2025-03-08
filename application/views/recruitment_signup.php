a790cdc848301f723d5f875e5a84b0d59ee2b4f9
Boros Szabolcs <borosszabolcs@yahoo.com>
2025-03-08 17:23:00 +0200

    skeleton for signup page view

--- application/views/recruitment_signup.php
+++ application/views/recruitment_signup.php
@@ -1,4 +1,49 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-						SIGN UP
+
+						<h2>SIGN UP</h2>
+
+<div class="container">
+	<?php echo form_open('account/signup','role="form" class="form-horizontal"'); ?>
+
+		<div class="form-group">
+		</div>
+		
+		<div class="form-group">
+		</div>
+		
+		<div class="form-group">
+		</div>
+		
+		<div class="form-group">
+		</div>
+		
+		<div class="form-group">
+		</div>
+		
+		<div class="form-group">
+		</div>
+		
+		<div class="form-group">
+		</div>
+		
+		<div class="form-group">
+		</div>
+		
+		<div class="form-group">
+		</div>
+		
+		<div class="form-group">
+		</div>
+		
+		<div class="form-group">
+			<div class="col-sm-offset-2 col-sm-10">
+				<button type="submit" class="btn btn-default btn-block">SIGN UP</button>
+			</div>
+		</div>
+		
+	<?php echo form_close(); ?>
+
+</div>
+
