a6e85214f1cb0fc78c935488193832843c54bac3
Boros Szabolcs <borosszabolcs@yahoo.com>
2025-03-08 17:24:28 +0200

    changed text

--- application/views/recruitment_footer.php
+++ application/views/recruitment_footer.php
@@ -6,6 +6,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			</div>
 			<div class="row">
 				<div class="col-md-12">
-					&copy; 2025 Boros Szabolcs <a href="https://github.com/boros-szabolcs/azteck314">(GitHub)</a>
+					&copy; 2025 Boros Szabolcs. <?php echo anchor('https://github.com/boros-szabolcs/azteck314', 'For Ungvari Csongor Istvan'); ?>
 				</div>
 			</div>
