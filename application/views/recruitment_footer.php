c9f2e8be29c20963e272324abae847307e3438ed
Boros Szabolcs <borosszabolcs@yahoo.com>
2025-03-08 03:08:00 +0200

    changed footer texxt
    included a github link as well

--- application/views/recruitment_footer.php
+++ application/views/recruitment_footer.php
@@ -6,6 +6,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			</div>
 			<div class="row">
 				<div class="col-md-12">
-					FOOTER
+					&copy; 2025 Boros Szabolcs <a href="https://github.com/boros-szabolcs/azteck314">(GitHub)</a>
 				</div>
 			</div>
