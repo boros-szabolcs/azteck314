2d6dd3a1001740da76e7dcec7e918e70a954ce19
Boros Szabolcs <borosszabolcs@yahoo.com>
2025-03-09 01:33:06 +0200

    added firstname and alias to page header
    also included a dropdown select box

--- application/views/recruitment_header.php
+++ application/views/recruitment_header.php
@@ -1,11 +1,27 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-			<div class="row">
-				<div class="col-md-12">
-					<div class="page-header">
-						<h1>RECRUITMENT</h1>
-					</div>
+			<div class="row page-header">
+				<div class="col-md-9">
+					<h1>RECRUITMENT</h1>
+				</div>
+				<div class="col-md-3 text-right">
+					<?php
+					if ($this->session->userdata('user'))
+					{
+						$user = $this->session->userdata('user');
+						if (is_array($user)) { $user = (object)$user; }
+						echo '<span>Welcome <strong>'.$user->firstname.' ('.$user->username.')</strong></span>';
+						$options = array('1'=>"egy", '2'=>"ketto", '3'=>"harom", '4'=>"negy", );
+						$attrbts = array(
+							'id' => 'id',
+							'class' => 'form-control',
+							'onChange' => 'alert(this.options[this.selectedIndex].text);',
+						);
+						echo form_dropdown('name', $options, '3', $attrbts);
+					}
+					?>
+
 				</div>
 			</div>
 			<div class="row">
