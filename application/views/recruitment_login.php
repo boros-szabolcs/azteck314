54e423a0dcf8ce57b2e163b858f4f4d3967b3102
Boros Szabolcs <borosszabolcs@yahoo.com>
2025-03-08 16:54:07 +0200

    implemented login page view

--- application/views/recruitment_login.php
+++ application/views/recruitment_login.php
@@ -1,4 +1,46 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-						LOG IN
+
+						<h2>LOG IN</h2>
+
+<div class="container">
+	<?php echo form_open('account/login','role="form" class="form-horizontal"'); ?>
+
+		<div class="form-group">
+			<?php echo form_label('E-mail','email','class="control-label col-sm-2"'); ?>
+
+			<div class="col-sm-10">
+				<input type="email" id="email" name="email" placeholder="E-mail" class="form-control" />
+			</div>
+		</div>
+		
+		<div class="form-group">
+			<?php echo form_label('Password','password','class="control-label col-sm-2"'); ?>
+
+			<div class="col-sm-10">
+				<input type="password" id="password" name="password" placeholder="Password" class="form-control" />
+			</div>
+		</div>
+		
+		<div class="form-group">
+			<div class="col-sm-offset-2 col-sm-10">
+				<button type="submit" class="btn btn-default btn-block">LOG IN</button>
+			</div>
+		</div>
+		
+	<?php echo form_close(); ?>
+
+</div>
+
+<div class="container">
+	<?php
+		echo form_open();
+		echo form_hidden('navigate', 'signup');
+		echo 'Already have an account?';
+		echo form_submit('submit', 'SIGN UP', 'class="btn btn-link"');
+		echo form_close();
+	?>
+
+</div>
+
