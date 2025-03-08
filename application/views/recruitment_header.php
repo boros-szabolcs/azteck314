541e578ee905c2595882903d5f5252cddcc24ba7
Boros Szabolcs <borosszabolcs@yahoo.com>
2025-03-08 18:42:55 +0200

    changed text

--- application/views/recruitment_header.php
+++ application/views/recruitment_header.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			<div class="row">
 				<div class="col-md-12">
 					<div class="page-header">
-						<h1>HEADER</h1>
+						<h1>RECRUITMENT</h1>
 					</div>
 				</div>
 			</div>
